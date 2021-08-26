1222846c7efc09cab36070834b115fbc02bf49c6
Dan Smith <dev@dotherightthing.co.nz>
2021-08-26 19:25:14 +1200

    Rename macros, refactor loop led user action

--- korg-nanokontrol-studio/clyphx-pro/user_actions/SelectedTrackLedStates.py
+++ korg-nanokontrol-studio/clyphx-pro/user_actions/SelectedTrackLedStates.py
@@ -15,28 +15,24 @@ TRACK_SELECT_1 = CC, 1, 46, 0, 127, 1/SEL
 
 X-Controls can trigger actions, LED can be lit by setting the CC value to 127, but implementation is rough.
 For example, use of wait is required to reinstate the lit LED after the button which sets the state is released.
-X_TRACK_SELECT_1 = CC, 1, 46, 0, 127, 1/sel ; update_select_leds MIDI CC 1, 46, 47, 48, 49, 50, 51, 52, 53 ; update_loop_led MIDI CC 1 55
+X_TRACK_SELECT_1 = CC, 1, 46, 0, 127, 1/sel ; update_select_leds MIDI CC 1, 46, 47, 48, 49, 50, 51, 52, 53 ; loop_led
 
 X-Clips can trigger actions like X-Controls, but are verbose and difficult to edit.
 
 SOLUTION:
 
-Button Bindings.txt maps the eight 'Select' MIDI controls to CXP's track SEL actions. This provides the best LED control.
-TRACK_1_SELECT = CC, 1, 46, 0, 127, 1/SEL
-TRACK_2_SELECT = CC, 1, 47, 0, 127, 2/SEL
-TRACK_3_SELECT = CC, 1, 48, 0, 127, 3/SEL
-TRACK_4_SELECT = CC, 1, 49, 0, 127, 4/SEL
-TRACK_5_SELECT = CC, 1, 50, 0, 127, 5/SEL
-TRACK_6_SELECT = CC, 1, 51, 0, 127, 6/SEL
-TRACK_7_SELECT = CC, 1, 52, 0, 127, 7/SEL
-TRACK_8_SELECT = CC, 1, 53, 0, 127, 8/SEL
+G-Controls.txt maps the eight 'Select' MIDI controls to CXP's track SEL actions, with reliable LED control
+and actions that leverage the simplicity of CXP macros and variables
+
+Additional selection functionality uses user_actions which can also access variables.
+Alternatively on_selected_track_changed(self) can be used, but this obscures what is happening.
 
 X-Controls.txt maps the 'Set' MIDI control to CXP's CLIP LOOP action.
 (Specifying on and off actions prevents having to push the button twice for each state).
 CLIP_LOOP = CC, 1, 55, 0, 127, SEL/CLIP LOOP ON : SEL/CLIP LOOP OFF
 
 on_selected_track_changed(self) calls extra actions whenever a track is selected in Live.
-update_loop_led() is called here to update the lit state of the 'Set' control's LED.
+loop_led() is called here to update the lit state of the 'Set' control's LED.
 
 KORG KONTROL Editor sets the corresponding Button Behaviors (for nanoKONTROL Studio):
 * 'clip loop' button/indicator = 'Set' button = Button Behavior = Toggle
@@ -77,15 +73,15 @@ class SelectedTrackLedStates(UserActionsBase):
         self.add_clip_action('deck_leds', self.deck_leds)
 
         # called by action_if_live_onrelease
-        self.add_clip_action('update_loop_led_clip', self.update_loop_led)
+        self.add_clip_action('loop_led_clip', self.loop_led)
 
         # called by on_selected_track_changed()
-        self.add_track_action('update_loop_led', self.update_loop_led)
+        self.add_track_action('loop_led', self.loop_led)
 
 
-    def on_selected_track_changed(self):
-        empty_dict = {}
-        self.update_loop_led(empty_dict, 'MIDI CC 1 55')
+    # def on_selected_track_changed(self):
+    #     empty_dict = {}
+    #     self.my_action(empty_dict, 'my_str')
 
     def action_if_live_onpress(self, action_def, args):
         # the current Live set object
@@ -98,36 +94,35 @@ class SelectedTrackLedStates(UserActionsBase):
 
         # tracks 1-4 are Traktor
         # tracks 5-8 are Live
-        if (selected_track_name != '1') and (selected_track_name != '4'):
+        if (selected_track_name != '1') and (selected_track_name != '1FX') and (selected_track_name != '4FX') and (selected_track_name != '4'):
             self.canonical_parent.clyphx_pro_component.trigger_action_list(action_list)
 
     def action_if_live_onrelease(self, action_def, args):
-        action_list = 'wait 1 ; user_clip update_loop_led_clip MIDI CC 1 55'
+        action_list = 'wait 1 ; user_clip loop_led_clip MIDI %MIDI_CC_LOOP%'
         self.canonical_parent.clyphx_pro_component.trigger_action_list(action_list)
 
     # when a track is selected
     # highlight the loop ('set') button LED
     # if the track contains a playing clip which is looping
     # there is no Button Binding for this, so a user action is required
-    def update_loop_led(self, action_def, args):
+    def loop_led(self, action_def, args):
         # the current Live set object
         # see also https://docs.cycling74.com/max8/vignettes/live_object_model
         live_set = self.song()
 
-        cc = args
         song_view = live_set.view
         selected_track = song_view.selected_track
         selected_track_name = selected_track.name
         action_list = ''
-        action_list_off = cc + ' 0' # MIDI CC X Y 0
-        action_list_on = cc + ' 127' # MIDI CC X Y 127
+        action_list_off = 'MIDI %MIDI_CC_LOOP% 0'
+        action_list_on = 'MIDI %MIDI_CC_LOOP% 127'
 
         # see https://docs.cycling74.com/max8/vignettes/live_object_model > playing_slot_index
         clip_stop_slot_fired_in_session_view = -2
 
         # tracks 1-4 are Traktor
         # tracks 5-8 are Live
-        if (selected_track_name != '1') and (selected_track_name != '4'):
+        if (selected_track_name != '1') and (selected_track_name != '1FX') and (selected_track_name != '4FX') and (selected_track_name != '4'):
             # track = action_def['track'] # not working
             tracks = list(live_set.tracks)
             selected_track_index = list(tracks).index(selected_track)
@@ -144,7 +139,7 @@ class SelectedTrackLedStates(UserActionsBase):
             else:
                 action_list += action_list_off
 
-        # self.canonical_parent.log_message('update_loop_led: ' + action_list)
+        # self.canonical_parent.log_message('loop_led: ' + action_list)
         self.canonical_parent.clyphx_pro_component.trigger_action_list(action_list)
 
     def deck_leds(self, action_def, args):
